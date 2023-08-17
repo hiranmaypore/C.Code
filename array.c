d59515f293627ffbefb83d8abc6e72a0a5b66e3f
hiranmaypore <141160617+hiranmaypore@users.noreply.github.com>
2023-08-17 12:14:56 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a[20],i,n;
+	printf("enter the number of n\n");
+	scanf("%d",&n);
+	printf("enter the elements\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);{
+	printf("print elements\n");}
+	for(i=0;i<n;i++)
+	printf("%d\n",a[i]);
+	return 0;
+}
